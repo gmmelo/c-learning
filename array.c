adc839a0245db7ebdc7ca59ff2903cabb1a49113
Gael Melo <104900699+gmmelo@users.noreply.github.com>
2023-12-26 19:27:07 -0700

    add array test

---
+++ array.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+// Counting digits (0-9), white space, and others in order to test arrays
+int main()
+{
+    int c, nwhite, nother;
+    int ndigit[10];
+
+    nwhite = nother = 0;
+    for (int i = 0; i < 10; ++i)
+        ndigit[i] = 0;
+
+    while ((c = getchar()) != EOF)
+        if (c >= '0' && c <= '9')
+            ++ndigit[c - '0'];
+        else if (c == ' ' || c == '\n' || c == '\n' || c == '\t')
+            ++nwhite;
+        else
+            ++nother;
+
+    printf("digits =");
+    for (int i = 0; i < 10; ++i)
+        printf(" %d", ndigit[i]);
+    printf(", white space = %d, other = %d\n",
+        nwhite, nother);
+}
